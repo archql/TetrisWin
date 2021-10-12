0e81808621001b62759e2d0bee784b265d0bc98d
archql <59373768+archql@users.noreply.github.com>
2021-10-12 19:23:31 +0300

    My First try to mov tetris for DOS to Windows 10
    - all 16 bit logics converted to 32 bit
    - winapi & opengl configured

---
+++ units/Random.h
@@ -0,0 +1,41 @@
+;------------------------------------------------
+; initialize random using cur time
+Random.Initialize:
+        ;mov     ah, 2Ch; get sys time
+        ;int     21h    ; got seed in dx  (s and ms*10)   ???
+        invoke GetTickCount ; tick time in rax
+
+        mov     [Random.dPrewNumber], eax
+
+        ret
+
+proc Random.Get uses ecx edx,\; not nessesary to save cx dx
+        dMin, dMax; dBounds - wMax'wMin
+
+        ; pseudo random generator (A*x + B) mod N
+        mov     eax, [Random.dPrewNumber]
+
+        mov     ecx, 29;; A
+        mul     ecx
+        add     eax, 47;; B
+        ;xor     edx, edx clear after mul
+
+        mov     [Random.dPrewNumber], eax
+
+        mov     ecx, [dMax]
+        mov     edx, [dMin]
+        sub     ecx, edx
+        inc     ecx
+        xor     edx, edx
+        div     ecx
+
+        mov     eax, edx
+        mov     edx, [dMin]; wMin
+        add     eax, edx
+
+        ret
+endp
+
+
+;============other data=============
+Random.dPrewNumber      dd      ?
